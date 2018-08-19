dec98e312d7c303241a30a64436d7ddfb8ac764e
McKenzie Julia Lantier <40756100+mechroCutie@users.noreply.github.com>
2018-08-19 16:51:38 -0700

    fixed typo

--- shell/zsh/functions.zsh
+++ shell/zsh/functions.zsh
@@ -2,6 +2,6 @@ weather () { curl wttr.in/"$@"; }
 cheat () { curl cheat.sh/"$@"; }
 0x0 () { curl -F'file=@"$@"' http://0x0.st; }
 isdown () {
-curl -s http://daownforeveryoneorjustme.com/"$@"|grep -e 
+curl -s http://downforeveryoneorjustme.com/"$@"|grep -e 
 's/<\/span>//;s/\(.*[\.!]\).*<a.*>\(.*\)<\/a>\(.*\)/\1 \2\3/;s/^ *//':
 }
